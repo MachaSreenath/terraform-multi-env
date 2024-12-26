4549db7f8555d0d9e136c36ec9b30dcba734e855
MachaSreenath <sreenathshree9@gmail.com>
2024-12-26 19:30:21 +0530

    terraform changes

--- workspaces/ec2.tf
+++ workspaces/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0b4f379183e5706b9" #devops-practice
-  instance_type = var.instance_type
+  instance_type = lookup(var.instance_type, terraform.workspace)
 
   tags = {
     Name = "HelloTerraform"
